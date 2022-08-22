c6b6a30464691e401071f16b03c550b0b479e266
Sehajpreet Singh <sehajmagan@gmail.com>
2022-08-22 01:59:37 -0400

    added logic to handle css for redirect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import { Fragment } from "react";
+import MainNavbar from "./components/main-navbar/main-navbar.component";
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Fragment>
+      <MainNavbar />
+    </Fragment>
+  );
 }
 
 export default App;
