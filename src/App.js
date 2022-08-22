7d1e9ab2fe4eb74b3d4add5b211711fc6c8300d0
Sehajpreet Singh <sehajmagan@gmail.com>
2022-08-22 03:50:01 -0400

    updated functinality to handle multiple routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import { Fragment } from "react";
+import { Routes, Route } from "react-router-dom";
 import MainNavbar from "./components/main-navbar/main-navbar.component";
+import PersonalNavbar from "./components/personal-navbar/personal-navbar.component";
+import PhNavbar from "./components/ph-navbar/ph-navbar.component";
+
+function Personal() {
+  return <h1>I am Personal</h1>;
+}
+function Business() {
+  return <h1>I am Business</h1>;
+}
+function PV() {
+  return <h1>I am privatehealth</h1>;
+}
 function App() {
   return (
-    <Fragment>
-      <MainNavbar />
-    </Fragment>
+    <Routes>
+      <Route path="/" element={<MainNavbar />}>
+        <Route path="personal" element={<PersonalNavbar />}>
+          <Route key="ba" path="ba" element={<Personal />} />
+          <Route path="cc" element={<PV />} />
+          <Route path="m" element={<Business />} />
+        </Route>
+        <Route path="privatehealth" element={<PhNavbar />}>
+          <Route key="ba" path="ba" element={<Personal />} />
+          <Route path="cc" element={<PV />} />
+        </Route>
+        {/* to do */}
+        <Route path="privatehealth" element={<PersonalNavbar />} />
+        <Route path="business" element={<PersonalNavbar />} />
+        <Route path="commercial" element={<PersonalNavbar />} />
+        <Route path="capitalmarkets" element={<PersonalNavbar />} />
+        <Route path="GAM" element={<PersonalNavbar />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/main-navbar/main-navbar.component.jsx
+++ src/components/main-navbar/main-navbar.component.jsx
@@ -1,7 +1,7 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useEffect, useState } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { Disclosure } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -10,12 +10,53 @@ function classNames(...classes) {
 function MainNavbar() {
   const [currentItem, setCurrentItem] = useState(true);
   const [navigation, SetNavigation] = useState([
-    { name: "Personal", href: "personal", current: false },
-    { name: "Private Health", href: "privatehealth", current: false },
-    { name: "Business", href: "business", current: false },
-    { name: "Commercial", href: "commercial", current: false },
-    { name: "Capital Markets", href: "capital-markets", current: false },
-    { name: "Global Asset Management", href: "GAM", current: false },
+    {
+      name: "Personal",
+      href: "personal",
+      current: true,
+      subitem: [
+        { name: "Bank Accounts", href: "ba", current: true },
+        { name: "CreditCards", href: "cc", current: false },
+        { name: "Mortgages", href: "m", current: false },
+        { name: "Loans & Lines of Credit", href: "llc", current: false },
+      ],
+    },
+    {
+      name: "Private Health",
+      href: "privatehealth",
+      current: false,
+      subitem: [
+        { name: "Bank Accounts", href: "ba", current: false },
+        { name: "CreditCards", href: "cc", current: false },
+      ],
+    },
+    {
+      name: "Business",
+      href: "business",
+      current: false,
+      subitem: [
+        { name: "Mortgages", href: "m", current: false },
+        { name: "Loans & Lines of Credit", href: "llc", current: false },
+      ],
+    },
+    {
+      name: "Commercial",
+      href: "commercial",
+      current: false,
+      subitem: [{ name: "Bank Accounts", href: "ba", current: false }],
+    },
+    {
+      name: "Capital Markets",
+      href: "capitalmarkets",
+      current: false,
+      subitem: [{ name: "Mortgages", href: "m", current: false }],
+    },
+    {
+      name: "Global Asset Management",
+      href: "GAM",
+      current: false,
+      subitem: [{ name: "CreditCards", href: "cc", current: false }],
+    },
   ]);
   function handleClick(item) {
     item.current = currentItem;
@@ -35,76 +76,83 @@ function MainNavbar() {
     SetNavigation(newNavigation);
   }
   return (
-    <Disclosure as="nav" className="bg-bmoblue">
-      {({ open }) => (
-        <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
+    <>
+      <Disclosure as="nav" className="bg-bmoblue">
+        {({ open }) => (
+          <>
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+              <div className="relative flex items-center justify-between h-16">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
 
-              {/* desktop items */}
-              {/* left items div */}
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-white text-black"
-                            : "text-gray-300 hover:bg-white hover:text-black",
-                          "px-6 py-6 text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                        onClick={() => handleClick(item)}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+                {/* desktop items */}
+                {/* left items div */}
+                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="hidden sm:block sm:ml-6">
+                    <div className="flex space-x-4">
+                      {navigation.map((item) => (
+                        <Link
+                          key={item.href}
+                          to={{
+                            pathname: item.href,
+                          }}
+                          state={{ navitems: item.subitem }}
+                          className={classNames(
+                            item.current
+                              ? "bg-white text-black"
+                              : "text-gray-300 hover:bg-white hover:text-black",
+                            "px-6 py-6 text-sm font-medium"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                          onClick={() => handleClick(item)}
+                          replace={true}
+                        >
+                          {item.name}
+                        </Link>
+                      ))}
+                    </div>
                   </div>
                 </div>
+                {/* right items div */}
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"></div>
               </div>
-              {/* right items div */}
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"></div>
             </div>
-          </div>
 
-          {/* Mobile menu items*/}
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block px-3 py-2 rounded-md text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+            {/* Mobile menu items*/}
+            <Disclosure.Panel className="sm:hidden">
+              <div className="px-2 pt-2 pb-3 space-y-1">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      "block px-3 py-2 rounded-md text-base font-medium"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+      <Outlet />
+    </>
   );
 }
 
